2bda8db7b1aa2cfd1893effbddcb42cfc86ba0d1
Ryan Irwin <rirwin@Ryans-MacBook-Air.local>
2013-11-03 21:53:34 -0500

    Removed c++ 0x style auto variables.
    Simplified how a run is called in main.  It converges when a round
    is not improved by a small delta.
    
    Added argument parsing.
    
    Added a test for comparing the costs to the number of centroids.
    more to come in this...

--- k-cluster/main.cpp
+++ k-cluster/main.cpp
@@ -4,9 +4,18 @@
 #include <assert.h> 
 #include <math.h>
 #include <cfloat>
-#define MAX_VAL 200
+#include <getopt.h>
+
+const int max_pt_val = 200;
 
 using namespace std;
+
+/**
+ * Parsing/usage functions
+ */
+char *progname;
+static void usage();
+static void parse_args (int argc, char *argv[], int *num_points, int *num_dimensions, double *round_delta, int *num_clusters, int *rng_seed);
 
 
 class Point {
@@ -57,6 +66,11 @@ private:
   int num_dim;
 
   /**
+   *
+   */
+  int rng_seed;
+
+  /**
    * number of points
    */
   int num_pts;
@@ -87,7 +101,18 @@ private:
    */
   int num_iter;
 
+  /**
+   * small change in cost required to continue iterations
+   */
+  double round_delta;
+
 public:
+
+  ClusteringDriver(int k_a, double round_delta_a) {
+    k = k_a;
+    round_delta = round_delta_a;
+    num_iter = 0;
+  }
 
   ClusteringDriver(int k_a) {
     k = k_a;
@@ -96,6 +121,17 @@ public:
 
   ClusteringDriver() {
     num_iter = 0;
+    k = 2;
+  }
+
+  double get_final_cost() {
+
+    return clust_costs[clust_costs.size()-1];
+  }
+  
+  int get_num_rounds() {
+
+    return clust_costs.size()-1;
   }
 
   void print_round() {
@@ -108,22 +144,62 @@ public:
 
     cout << "---- Round " << num_iter << " ---- " << endl;
     cout << "centroids are at: " << endl;
-    for (auto &cent_i : centroids) {
-      cent_i.print_point();
+    for (int i = 0; i < centroids.size(); i++) {
+      centroids[i].print_point();
     }
+
+    // c++-0x, OS X does not have this compiler :(
+    //    for (auto &cent_i : centroids) {
+    //      cent_i.print_point();
+    //    }
 
     cout << "cost is: " << clust_costs[num_iter-1] << endl;
     cout << endl;
 
   }
 
-  void gen_rnd_pts(int num_pts_a, int num_dim_a) {
+  void run() {
+
+    perform_initial_association();
+    int prev_cost = clust_costs[clust_costs.size()-1];
+    perform_round();
+    int curr_cost = clust_costs[clust_costs.size()-1];
+    while (curr_cost < prev_cost - round_delta) {
+      prev_cost = curr_cost;
+      perform_round();
+      curr_cost = clust_costs[clust_costs.size()-1];
+    }
+  }
+
+
+  void perform_initial_association() {
+
+    init_centroids();
+    associate_pts();
+    //    print_round();
+  }
+
+
+  void perform_round() {
+
+    recompute_centroids();
+    associate_pts();
+    //    print_round();
+  }
+
+
+
+
+  void gen_unif_rnd_pts(int num_pts_a, int num_dim_a, int rng_seed_a) {
 
     num_pts = num_pts_a;
     num_dim = num_dim_a;
+    rng_seed = rng_seed_a;
+
+    srand(rng_seed);
     
     for (int i = 0; i < num_pts; i++) {
-      pts.push_back(gen_rand_pt());
+      pts.push_back(gen_unif_rnd_pt());
     }
     assoc.resize(num_pts);
   }
@@ -145,11 +221,17 @@ public:
     assert(clust_costs.size() == num_iter);
     clust_costs.push_back(0);
 
-    for (auto &pt_i : pts) {
-      int pt_idx = &pt_i - &pts[0];
-      int cent_idx = get_nearest_centroid_idx(pt_i);
-      assoc[pt_idx] = cent_idx;
+    for (int pt_i = 0; pt_i < pts.size(); pt_i++) {
+      int cent_idx = get_nearest_centroid_idx(pts[pt_i]);
+      assoc[pt_i] = cent_idx;
     }
+
+    // C++ 0x
+    //    for (auto &pt_i : pts) {
+    //      int pt_idx = &pt_i - &pts[0];
+    //      int cent_idx = get_nearest_centroid_idx(pt_i);
+    //      assoc[pt_idx] = cent_idx;
+    //    }
 
     num_iter++;
   }
@@ -172,7 +254,8 @@ public:
     vector<double> sums;
     int ctr = 0;
     sums.resize(num_dim);
-    for (auto &i : sums){i = 0;}
+    //    for (auto &i : sums){i = 0;} // c++-0x
+    for (int i = 0; i < sums.size(); i++){sums[i] = 0;}
 
     for (int pt_i = 0; pt_i < num_pts; pt_i++) {
       if (assoc[pt_i] == cent_idx) {
@@ -204,23 +287,45 @@ public:
     double min_dist = DBL_MAX;
     int cent_idx = -1;
 
-    for (auto &cent_i : centroids){
-      auto idx = &cent_i - &centroids[0];
-      double dist = compute_distance(pt, cent_i);
+    for (int idx = 0; idx < centroids.size(); idx++){
+      double dist = compute_distance(pt, centroids[idx]);
       if (dist < min_dist) {
 	cent_idx = idx;
 	min_dist = dist;
       }
     }
+
+    // c++ 0x
+    //    for (auto &cent_i : centroids){
+    //      auto idx = &cent_i - &centroids[0];
+    //      double dist = compute_distance(pt, cent_i);
+    //      if (dist < min_dist) {
+    //	cent_idx = idx;
+    //	min_dist = dist;
+    //      }
+    //    }
+
     clust_costs[num_iter] += min_dist;
     
     return cent_idx;
   }
 
-  Point gen_rand_pt() {
+  Point gen_bimodal_gauss_pt() {
     vector<double> vals;
     for (int i = 0; i < num_dim; i++) {
-      vals.push_back(rand() % MAX_VAL);
+      vals.push_back(normal());
+    }
+    
+    Point pt(vals);
+
+    return pt;
+  }
+
+
+  Point gen_unif_rnd_pt() {
+    vector<double> vals;
+    for (int i = 0; i < num_dim; i++) {
+      vals.push_back(rand() % max_pt_val);
     }
     
     Point pt(vals);
@@ -232,29 +337,136 @@ public:
 
 
 
+struct Result{
+  int rng_seed;
+  int num_pts;
+  int num_dim;
+  int k_centroids;
+  int num_rounds;
+  double final_cost;
+};
 
+int main(int argc, char* argv[]) {
 
-int main() {
-
-  ClusteringDriver *cd;
+  int k_centroids, num_dim, num_pts, rng_seed, num_runs;
+  double round_delta;
   
-  int k_centroids = 4;
-  int num_dim = 2;
-  int num_pts = 100;
-  int num_rounds = 10;
+  parse_args(argc, argv, &num_pts, &num_dim, &round_delta, &k_centroids, &rng_seed);
 
-  cd = new ClusteringDriver(k_centroids);
-  cd->gen_rnd_pts(num_pts, num_dim);
-  cd->init_centroids();
-  cd->associate_pts();
-  cd->print_round();
+  num_runs = 10;
+  
+  vector<Result> results;
 
+  ClusteringDriver *cd;  
 
-  for (int i = 0; i < num_rounds; i++) {
-    cd->recompute_centroids();
-    cd->associate_pts();
-    cd->print_round();
+  for (int k_centroids_i = k_centroids;  k_centroids_i < k_centroids + num_runs; k_centroids_i++) {
+  
+    cd = new ClusteringDriver(k_centroids_i, round_delta);
+
+    // generate pts (uniform) randomly
+    cd->gen_rnd_pts(num_pts, num_dim, rng_seed);
+    
+    // runs until completion
+    cd->run();
+    Result res;
+    res.num_rounds = cd->get_num_rounds();
+    res.final_cost = cd->get_final_cost();
+    res.num_pts = num_pts;
+    res.num_dim = num_dim;
+    res.k_centroids = k_centroids_i;
+    res.rng_seed = rng_seed;
+    
+    results.push_back(res);
+    cout << "------" << endl;
+    cout << "final cost " << res.final_cost << endl;
+    cout << "num centroids " << res.k_centroids << endl;
+    cout << "num rounds " << res.num_rounds << endl;
+    cout << "------" << endl << endl;
+
+    delete cd;
   }
 
+  
+
   return 0;
+}
+
+
+/**
+ * handles arguments in long form
+ */
+static struct option longopts [] =
+  {
+    { "help",          no_argument,       NULL, 'h' },
+    { "num-points",     required_argument, NULL, 'p' },
+    { "num-dimensions",   required_argument, NULL, 'd' },
+    { "round-delta",   required_argument, NULL, 'r' },
+    { "num-clusters",   required_argument, NULL, 'k' },
+    { "rng-seed",   required_argument, NULL, 's' }
+  };
+
+static void usage(){
+  printf("usage: %s -p <int> -d <int> -k <int> -r <double> -s <int>\n", progname);
+}
+
+/**
+ * Parses arguments for frequency and number of queries
+ */
+static void parse_args (int argc, char *argv[], int *num_points, int *num_dimensions, double *round_delta, int *num_clusters, int *rng_seed) {
+  progname = argv[0];
+  int opt;
+  *num_points = 0;
+  *num_dimensions = 0;
+  *round_delta = 0;
+  *num_clusters = 0;
+  *rng_seed = 0;
+  while(opt = getopt_long(argc, argv, "h:p:d:k:s:r:", 
+			  longopts, (int*)NULL), opt != EOF) {
+    switch(opt) {
+    case 'h':
+      usage();
+      break;
+    case 'p':
+      *num_points = atoi(optarg);
+      if(*num_points <= 0) usage();
+      break;
+    case 'd':
+      *num_dimensions = atoi(optarg);
+      if(*num_dimensions <= 0) usage();
+      break;
+    case 'r':
+      *round_delta = atof(optarg);
+      if(*round_delta <= 0) usage();
+      break;
+    case 'k':
+      *num_clusters = atoi(optarg);
+      if(*num_clusters <= 0) usage();
+      break;
+    case 's':
+      *rng_seed = atoi(optarg);
+      if(*rng_seed < 0) usage();
+      break;
+
+    default:
+      usage();
+      break;
+    }
+  }
+
+
+  if(*num_points <= 0) {
+    *num_points = 100; 
+  }
+  if(*num_dimensions <= 0) {
+    *num_dimensions = 2;
+  }
+  if(*round_delta <= 0) {
+    *round_delta = 0.00001;
+  }
+  if(*num_clusters <= 0) {
+    *num_clusters = 2;
+  }
+  if(*rng_seed < 0) {
+    *rng_seed = 0;
+  }
 }
