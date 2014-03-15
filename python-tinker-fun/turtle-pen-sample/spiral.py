b98fa659c618dce2a830500a29203fa3630fa481
Ryan Irwin <rirwin@Macintosh-2.local>
2014-03-15 17:11:57 -0400

    Enhanced spiral

--- python-tinker-fun/turtle-pen-sample/spiral.py
+++ python-tinker-fun/turtle-pen-sample/spiral.py
@@ -1,16 +1,24 @@
 import turtle
 t = turtle.Pen()
 
-fwd = 10
-
-colors = ['red','green','blue']
+fwd = 2
+size = 1
+colors = ['orange','red','pink','purple','green','blue','yellow']
 color_i = 0
-
-for i in range(0,10):
-    t.color(colors[i % 3])
+t.pensize(size)
+t.speed(0)
+for i in range(0,len(colors)*2):
+    t.color(colors[i % len(colors)])
     for j in range(0,6):
-        t.forward(fwd)
+        for k in range(0,5):
+            if (k % 2 == 0):
+                t.up()
+            else:
+                t.down()
+            t.forward(fwd)
         t.left(60)
         #fwd = fwd + 3
         fwd = fwd * 1.05
+        size = size * 1.02
+        t.pensize(size)
     #fwd = fwd + 10
