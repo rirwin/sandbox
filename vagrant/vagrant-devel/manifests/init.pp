584b2aee6e80d86ea31c5c59b199f80461d9acb7
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-04-26 21:34:53 -0400

    Adding a development environment.
    Added a few basic module

---
+++ vagrant/vagrant-devel/manifests/init.pp
@@ -0,0 +1,22 @@
+node default {
+
+  ## Defaults
+
+  # Always run apt-get update before trying to install packages
+  Package {
+    require => Exec["aptitude_update"],
+  }
+
+  $database_name = "devel"
+
+  $mysql_password = "mysql"
+
+  include "devel"
+}
+
+class devel {
+
+  include "aptitude"
+  include "emacs"
+  include "mysql"
+}
