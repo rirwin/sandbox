36a56576a64b9887e9088a01dfa610e2b751c519
Ryan Irwin <ryaneirwin@new-host-2.home>
2014-04-26 22:38:32 -0400

    reorganizing hadoop module

--- vagrant/modules/hadoop_0_20_2/manifests/init.pp
+++ vagrant/modules/hadoop_0_20_2/manifests/init.pp
@@ -1,22 +1,22 @@
-class hadoop {
+class hadoop_0_20_2 {
  $hadoop_home = "/opt/hadoop"
 
  exec { "download_hadoop":
- command => "wget -O /tmp/hadoop.tar.gz http://apache.mirrors.timporter.net/hadoop/common/hadoop-1.2.1/hadoop-1.2.1-bin.tar.gz",
+ command => "wget -O /tmp/hadoop.tar.gz http://archive.apache.org/dist/hadoop/core/hadoop-0.20.2/hadoop-0.20.2.tar.gz",
  path => $path,
- unless => "ls /opt | grep hadoop-1.2.1",
+ unless => "ls /opt | grep hadoop-0.20.2",
  require => Package["openjdk-6-jdk"]
 }
 
  exec { "unpack_hadoop" :
    command => "tar -zxf /tmp/hadoop.tar.gz -C /opt",
    path => $path,
-   creates => "${hadoop_home}-1.2.1",
+   creates => "${hadoop_home}-0.20.2",
    require => Exec["download_hadoop"]
  }
 
 file {
-  "${hadoop_home}-1.2.1/conf/slaves":
+  "${hadoop_home}-0.20.2/conf/slaves":
   source => "puppet:///modules/hadoop/slaves",
   mode => 644,
   owner => root,
@@ -25,7 +25,7 @@ file {
  }
 
 file {
-  "${hadoop_home}-1.2.1/conf/masters":
+  "${hadoop_home}-0.20.2/conf/masters":
   source => "puppet:///modules/hadoop/masters",
   mode => 644,
   owner => root,
@@ -34,7 +34,7 @@ file {
  }
 
 file {
-  "${hadoop_home}-1.2.1/conf/core-site.xml":
+  "${hadoop_home}-0.20.2/conf/core-site.xml":
   source => "puppet:///modules/hadoop/core-site.xml",
   mode => 644,
   owner => root,
@@ -43,7 +43,7 @@ file {
  }
  
 file {
-  "${hadoop_home}-1.2.1/conf/mapred-site.xml":
+  "${hadoop_home}-0.20.2/conf/mapred-site.xml":
   source => "puppet:///modules/hadoop/mapred-site.xml",
   mode => 644,
   owner => root,
@@ -52,7 +52,7 @@ file {
  }
  
  file {
-  "${hadoop_home}-1.2.1/conf/hdfs-site.xml":
+  "${hadoop_home}-0.20.2/conf/hdfs-site.xml":
   source => "puppet:///modules/hadoop/hdfs-site.xml",
   mode => 644,
   owner => root,
@@ -61,7 +61,7 @@ file {
  }
 
 file {
-  "${hadoop_home}-1.2.1/conf/hadoop-env.sh":
+  "${hadoop_home}-0.20.2/conf/hadoop-env.sh":
   source => "puppet:///modules/hadoop/hadoop-env.sh",
   mode => 644,
   owner => root,
