4254574606b1275cc6854bcb6ae7f4aa9dc5d19b
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-05-09 21:04:37 -0400

    Added an IntQueue class.

---
+++ datastructures/java/structures/IntQueue.java
@@ -0,0 +1,74 @@
+package structures;
+import java.util.*;
+
+public class IntQueue<T> {
+
+    // array container
+    private int[] a;
+
+    // i is head of queue, j is tail
+    private int i, j;
+
+    // size of queue
+    private int s;
+
+    private int numEnqueued;
+
+    public IntQueue(int s) {
+        this.i = 0;
+        this.j = 0;
+        this.a = new int[s];
+        this.s = s;
+        this.numEnqueued = 0;
+    }
+
+    public void push(int x) {
+        if ( j == i && numEnqueued > 0 ){
+            System.out.println("Queue is full");
+        }
+        else {
+            // place at tail, inc
+            a[j] = x;
+            j = (j + 1) % s;
+            numEnqueued++;
+        }
+    }
+
+    public void printContents() {
+        System.out.print("Contents: ");
+        for (int ii = 0; ii < this.s; ii++)
+            System.out.print(this.a[ii] + " ");
+        System.out.println();
+    }
+
+    public void printQueue() {
+        System.out.print("Queue: ");
+        for (int offset = 0; offset < this.numEnqueued; offset++)
+            System.out.print(this.a[(this.i + offset) % this.s] + " ");
+        System.out.println();
+    }
+
+    public int front() {
+        if (numEnqueued == 0) {
+            System.out.println("front(): Empty Queue");
+        }
+        return a[i];
+    }
+
+    public void pop() {
+
+        if (numEnqueued == 0) {
+            System.out.println("pop() Empty Queue");
+        }
+        else {
+            int x = a[i];
+            i = (i + 1) % s;
+            numEnqueued--;
+        }
+    }
+
+    public int get(int i) {
+        return a[i];
+    }
+
+}
