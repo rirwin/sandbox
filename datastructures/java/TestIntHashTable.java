7b920122d10ae150025caef3c1a1e4881c48ca99
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-05-17 10:37:33 -0400

    Added remove function. Added more test cases and asserts

--- datastructures/java/TestIntHashTable.java
+++ datastructures/java/TestIntHashTable.java
@@ -4,11 +4,54 @@ public class TestIntHashTable {
 
     public static void main(String[] args) {
         
-        int tableSize = 4093;
+        int tableSize = 4093; // large prime number
         IntHashTable iht = new IntHashTable(tableSize);
-        iht.printTable();
-        iht.insert(3,12);
-        iht.insert(13,90);
+
+        int key1 = 3, value1 = 12;
+        int key2 = 12, value2 = 143;
+        int key3 = tableSize+12, value3 = 99;
+        int key4 = 12, value4 = 1;
+        int key5 = tableSize+12, value5 = 2;
+        int key6 = 2*tableSize+12, value6 = 21;
+            
+        iht.put(key1,value1); 
+        iht.put(key2,value2); 
+
+        assert iht.hasKey(key1) == true;
+        assert iht.hasKey(key2) == true;
+
+        assert iht.get(key1) == value1;
+        assert iht.get(key2) == value2;
+
+        assert iht.getNumCollisions() == 0;
+
+        iht.put(key3,value3); 
+        iht.put(key4,value4);
+
+        assert iht.get(key3) == value3;
+        assert iht.get(key4) == value4;
+
+        assert iht.getNumCollisions() == 1;
+
+        iht.remove(key3);
+        assert iht.hasKey(key3) == false;
+        assert iht.get(key4) == value4;
+        assert iht.getNumCollisions() == 0;
+
+        iht.put(key5,value5); 
+        iht.put(key6,value6);
+        assert iht.getNumCollisions() == 2;
+
+        assert iht.get(key4) == value4;
+        assert iht.get(key5) == value5;
+        assert iht.get(key6) == value6;
+
+        iht.remove(key5);
+        assert iht.get(key4) == value4;
+        assert iht.hasKey(key5) == false;
+        assert iht.get(key6) == value6;
+        assert iht.getNumCollisions() == 1;
+
         iht.printTable();
     }
 }
