efb3778b8cba0e89dc45c1ca2f5e0ddf67b2e042
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-05-28 20:07:21 -0700

    Whitespace changes

--- datastructures/src/test/java/com/rirwin/structures/GenericQueueTest.java
+++ datastructures/src/test/java/com/rirwin/structures/GenericQueueTest.java
@@ -6,31 +6,33 @@ import org.junit.Test;
 
 public class GenericQueueTest {
 
-	@Test
-	public void testGenericQueue() {
+    @Test
+    public void testGenericQueue() {
 
-                final int queueSize = 100;
-		final GenericQueue<Integer> iq = new GenericQueue<>(queueSize);
+        final int queueSize = 100;
+        final GenericQueue<Integer> iq = new GenericQueue<>(queueSize);
 
-		assertEquals("Wrong number enqueued", 0, iq.getNumEnqueued());
+        assertEquals("Wrong number enqueued", 0, iq.getNumEnqueued());
 
-		final int first = 13;
-		iq.push(first);
+        final int first = 13;
+        iq.push(first);
 
-		assertEquals("First element not accepted", (int)first, (int)iq.front());
+        assertEquals("First element not accepted", (int)first, (int)iq.front());
 
-		for (int i = 0; i < queueSize*2; i++) {
-			iq.push(i);
-		}
+        for (int i = 0; i < queueSize*2; i++) {
+            iq.push(i);
+        }
 
-		assertEquals("First element disappeared", (int)first, (int)iq.front());
+        assertEquals("First element disappeared", (int)first, (int)iq.front());
 
-		for (int i = 0; i < queueSize-1; i++){
-			iq.pop();
-			assertEquals((int)i, (int)iq.front());
-		}
+        for (int i = 0; i < queueSize-1; i++){
+            iq.pop();
+            assertEquals((int)i, (int)iq.front());
+        }
 
                 
-                final GenericQueue<Double> dq = new GenericQueue<>(queueSize);
-        }
+        final GenericQueue<Double> dq = new GenericQueue<>(queueSize);
+
+                
+    }
 }
