714d60e0dbf7bdc30b69c3d8167c546ece104557
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-05-28 20:21:12 -0400

    Completed a Generic Queue class with the vector class

--- datastructures/src/main/java/com/rirwin/structures/GenericQueue.java
+++ datastructures/src/main/java/com/rirwin/structures/GenericQueue.java
@@ -1,19 +1,66 @@
 package com.rirwin.structures;
 
-import java.util.*;
+import java.util.Vector;
+import java.util.NoSuchElementException;
 
 public class GenericQueue<T> {
 
-    public Vector<T> v;
+    private final Vector<T> v;
 
-    public GenericQueue(int s) {
-        
+    private final int s;
+
+    private int numEnqueued;
+
+    public GenericQueue(int queueSize) {
         v = new Vector<T>();
-       
+        s = queueSize;
+        numEnqueued = 0;
     }
 
-    public T get(int i) {
-        return v.get(i);
+    public int getNumEnqueued() {
+        return numEnqueued;
     }
 
+    public void push(T elem) {
+
+        if (v.size() == s) {
+            //System.out.println("Queue is full");
+        }
+        else {
+            v.add(elem);
+            numEnqueued++;
+        }
+    }    
+
+    public void printContents() {
+        System.out.print("Contents: ");
+        for (int ii = 0; ii < numEnqueued; ii++) {
+            System.out.print(v.get(ii) + " ");
+        }
+        System.out.println();
+    }
+
+    public void printQueue() {
+        printContents();
+    }
+
+    public T front() {
+        if (numEnqueued == 0) {
+            throw new NoSuchElementException();
+        }
+        else {
+            return v.get(0);
+        }
+    }
+
+    public void pop() {
+        if (numEnqueued == 0) {
+            System.out.println("pop() Empty Queue");
+        }
+        else {;
+            v.remove(0);
+            numEnqueued--;
+        }
+    }
+    
 }
