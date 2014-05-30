b96a9084661ca515653e56ed8afe0fdfdf2f9434
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-05-30 08:56:29 -0700

    Example program for handling exceptions in a wrapper. Avoids needing a try-catch-finally in each function.

--- python-database-utils/common/database_manager.py
+++ python-database-utils/common/database_manager.py
@@ -1,14 +1,55 @@
 import psycopg2
 import sys
+import ConfigParser
+import json
+
+
+def dbwrap(func):
+    """Wrap a function in an idomatic SQL transaction.  The wrapped function
+    should take a cursor as its first argument; other arguments will be
+    preserved.
+    """
+
+    def new_func(conn, *args, **kwargs):
+
+        retval = None
+        cursor = conn.cursor()
+        try:
+            cursor.execute("BEGIN")
+            retval = func(cursor, *args, **kwargs)
+            cursor.execute("COMMIT")
+        except Exception, e:
+            cursor.execute("ROLLBACK")
+            print e
+            #raise
+        finally:
+            cursor.close()
+
+        return retval
+
+    # Tidy up the help()-visible docstrings to be nice
+    new_func.__name__ = func.__name__
+    new_func.__doc__ = func.__doc__
+
+    return new_func
+
+@dbwrap
+def do_something(cursor, val1=1, val2=2):
+    #cursor.execute("select %s, %s", (val1, val2))
+    cursor.execute("drop table foo")
+    return cursor.fetchall()
+
 
 class database_manager:
-    def __init__(self, config_path):
-        self.init_conn(config_path)
+
+    def __init__(self, db_conn_path, db_schema_path):
+        self.init_conn(db_conn_path)
+        self.schema_loader(db_schema_path)
+
         
-    def init_conn(self, config_path):
-        import ConfigParser
+    def init_conn(self, db_conn_path):
         config = ConfigParser.ConfigParser()
-        config.read(config_path)
+        config.read(db_conn_path)
         
         dbprog = config.get("main", "dbprog")
         if dbprog == "postgresql":
@@ -26,7 +67,45 @@ class database_manager:
                 print("%s \nCannot open a connection to database %s. \n Exiting.\n" % (e, psql_creds["database"]))
                 sys.exit(1)
         
-                
+
+    def schema_loader(self, db_schema_path):
+        try:
+            self.schema_dict = json.load(open(db_schema_path))
+        except Exception, e:
+            print("%s \nCannot load schema file %s. \n Exiting.\n" % (e, db_schema_path))
+            sys.exit(1)
+
+
+    def reset_all_tables(self):
+        for table_i in self.schema_dict:
+            self.reset_table(table_i)
+
+    def reset_table(self, table_str):
+        database_manager.drop_table(self.conn, table_str)
+
+
+    @staticmethod
+    @dbwrap
+    def drop_table(cursor, table_str):
+        #cursor.execute("drop table if exists " + table_str)
+        cursor.execute("drop table " + table_str)
+        return cursor.fetchall()[0][0]
+
+
+
+ 
 if '__main__' == __name__:
-    dm = database_manager("../conf/db_creds.conf")
-    
+
+    # Full path (incl. file name) to database credentials
+    db_conn_path = "../conf/db_creds.conf"
+
+    # Full path to database schema
+    db_schema_path = "../conf/db_schema.json"
+
+    dm = database_manager(db_conn_path, db_schema_path)
+
+    print do_something(dm.conn)
+
+    dm.reset_all_tables()
+
+    print "Program continues"
