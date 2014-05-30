3afebc3bd6eaf8ef89fa96132300cc1f2252a9a3
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-05-30 10:45:53 -0700

    Added loglevel in configuration

--- python-database-utils/common/database_manager.py
+++ python-database-utils/common/database_manager.py
@@ -3,7 +3,7 @@ import sys
 import ConfigParser
 import json
 import logging
-logging.basicConfig(level=logging.INFO)
+
 
 def dbwrap(func):
     """Wrap a function in an idomatic SQL transaction.  The wrapped function
@@ -45,30 +45,32 @@ def do_something(cursor, val1=1, val2=2):
 
 class database_manager:
 
-    def __init__(self, db_conn_path, db_schema_path):
-        self.init_conn(db_conn_path)
+    def __init__(self, db_config_path, db_schema_path):
+
+        self.load_config(db_config_path)
         self.schema_loader(db_schema_path)
 
-        
-    def init_conn(self, db_conn_path):
+
+    def load_config(self, db_config_path):
         config = ConfigParser.ConfigParser()
-        config.read(db_conn_path)
+        config.read(db_config_path)
         
         dbprog = config.get("main", "dbprog")
-        if dbprog == "postgresql":
-            psql_creds = dict(config.items("postgresql"))
+        if dbprog == "postgresql":        
+            self.init_psql_conn(config)
 
-            keys_needed = ["database","username","password","host","port"]
-            for key in keys_needed:
-                if key not in psql_creds:
-                    print "No", key, "in configuration file", config_path
-                    sys.exit(1)
+        logging.basicConfig(level = config.get("main", "loglevel") )
 
-            try:
-                self.conn = psycopg2.connect(database = psql_creds["database"], user = psql_creds["username"], password = psql_creds["password"], host = psql_creds["host"], port = psql_creds["port"])
-            except Exception, e:
-                print("%s \nCannot open a connection to database %s. \n Exiting.\n" % (e, psql_creds["database"]))
-                sys.exit(1)
+    # uncomment later and make this part of a package
+    # @general_exception_handler_wrapper 
+    def init_psql_conn(self, config):
+
+        psql_creds = dict(config.items("postgresql"))
+        try:
+            self.conn = psycopg2.connect(database = psql_creds["database"], user = psql_creds["username"], password = psql_creds["password"], host = psql_creds["host"], port = psql_creds["port"])
+        except Exception, e:
+            print("%s \nCannot open a connection to database %s. \n Exiting.\n" % (e, psql_creds["database"]))
+            sys.exit(1)
         
 
     def schema_loader(self, db_schema_path):
