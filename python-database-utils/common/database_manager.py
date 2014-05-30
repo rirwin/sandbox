a62907e8b36832891e8cf93f82aee68f5615cae4
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-05-30 15:16:32 -0700

    Added a separate wrappers.py file with classes and functions for different ways of handling exceptions

--- python-database-utils/common/database_manager.py
+++ python-database-utils/common/database_manager.py
@@ -3,40 +3,10 @@ import sys
 import ConfigParser
 import json
 import logging
+import wrappers
 
 
-def dbwrap(func):
-    """Wrap a function in an idomatic SQL transaction.  The wrapped function
-    should take a cursor as its first argument; other arguments will be
-    preserved.
-    """
-
-    def new_func(conn, *args, **kwargs):
-
-
-        retval = None
-        cursor = conn.cursor()
-        try:
-            cursor.execute("BEGIN")
-            retval = func(cursor, *args, **kwargs)
-            cursor.execute("COMMIT")
-        except Exception, e:
-            cursor.execute("ROLLBACK")
-            logging.info(str(e) + " printed via logging.info")
-            logging.warning(str(e) + " printed via logging.warning")
-            #raise
-        finally:
-            cursor.close()
-
-        return retval
-
-    # Tidy up the help()-visible docstrings to be nice
-    new_func.__name__ = func.__name__
-    new_func.__doc__ = func.__doc__
-
-    return new_func
-
-@dbwrap
+@wrappers.db_trx_exception_handler
 def do_something(cursor, val1=1, val2=2):
     #cursor.execute("select %s, %s", (val1, val2))
     cursor.execute("drop table foo")
@@ -46,9 +16,7 @@ def do_something(cursor, val1=1, val2=2):
 class database_manager:
 
     def __init__(self, db_config_path):
-
         self.load_config(db_config_path)
-
 
     def load_config(self, db_config_path):
         config = ConfigParser.ConfigParser()
@@ -56,25 +24,37 @@ class database_manager:
         
         # set up postgreSQL
         dbprog = config.get("main", "dbprog")
-        if dbprog == "postgresql":        
-            self.init_psql_conn(config)
+        if dbprog == "postgresql":
+            conn = self.init_psql_conn(config)
+            if conn is None:
+                logging.error("\nCannot open a connection to database. Exiting.\n")
+                sys.exit(1)
+            self.conn = conn
+
+            #database_manager.init_psql_conn_static(config)
             self.schema_loader(config)
 
         logging.basicConfig(level = config.get("main", "loglevel") )
 
 
+    def schema_loader(self, config):
+        try:
+            db_schema_path = config.get("main", "schemafile")
+            self.schema_dict = json.load(open(db_schema_path))
+        except Exception, e:
+            print("%s \nCannot load schema file %s. \n Exiting.\n" % (e, db_schema_path))
+            sys.exit(1)
 
-    # uncomment later and make this part of a package
-    # @general_exception_handler_wrapper 
+    @wrappers.logger
+    @wrappers.general_function_handler
     def init_psql_conn(self, config):
 
+        conn = None
         psql_creds = dict(config.items("postgresql"))
-        try:
-            self.conn = psycopg2.connect(database = psql_creds["database"], user = psql_creds["username"], password = psql_creds["password"], host = psql_creds["host"], port = psql_creds["port"])
-        except Exception, e:
-            print("%s \nCannot open a connection to database %s. \n Exiting.\n" % (e, psql_creds["database"]))
-            sys.exit(1)
-        
+
+        conn = psycopg2.connect(database = psql_creds["database"], user = psql_creds["username"], password = psql_creds["password"], host = psql_creds["host"], port = psql_creds["port"])
+
+        return conn
 
     def schema_loader(self, config):
         try:
@@ -90,17 +70,24 @@ class database_manager:
             self.reset_table(table_i)
 
     def reset_table(self, table_str):
-        database_manager.drop_table(self.conn, table_str)
+        self.drop_table(self.conn, table_str)
 
-
-    @staticmethod
-    @dbwrap
-    def drop_table(cursor, table_str):
+    # called by <referent>.drop_table(<referent>.conn, table_str)
+    @wrappers.logger
+    @wrappers.database_function_handler
+    def drop_table(self, cursor, table_str):
         #cursor.execute("drop table if exists " + table_str)
-        cursor.execute("drop table " + table_str)
+        cursor.execute("drop table " + table_str) # force exception
         return cursor.fetchall()[0][0]
 
 
+    # called by database_manager.drop_table_static(conn, table_str)
+    @staticmethod
+    @wrappers.db_trx_exception_handler
+    def drop_table_static(cursor, table_str):
+        #cursor.execute("drop table if exists " + table_str)
+        cursor.execute("drop table " + table_str) # force exception
+        return cursor.fetchall()[0][0]
 
  
 if '__main__' == __name__:
@@ -110,7 +97,7 @@ if '__main__' == __name__:
 
     dm = database_manager(db_config_path)
 
-    print do_something(dm.conn)
+    #print do_something(dm.conn)
 
     dm.reset_all_tables()
 
---
+++ python-database-utils/common/wrappers.py
@@ -0,0 +1,97 @@
+import logging
+
+
+class logger(object):
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, obj, type=None):
+        return self.__class__(self.func.__get__(obj, type))
+    def __call__(self, *args, **kw):
+        logging.info('Entering %s' % self.func)
+        return self.func(*args, **kw)
+
+
+class general_function_handler(object):
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, obj, type=None):
+        return self.__class__(self.func.__get__(obj, type))
+    def __call__(self, *args, **kw):
+        try:
+            retval = self.func(*args, **kw)
+        except Exception, e :
+            logging.warning('Exception in %s' % self.func)
+            template = "An exception of type {0} occured. Arguments:\n{1!r}"
+            message = template.format(type(e).__name__, e.args)
+            logging.exception(message)
+            retval = None
+        return retval
+    
+class database_function_handler(object):
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, obj, type=None):
+        return self.__class__(self.func.__get__(obj, type))
+    def __call__(self, conn, *args, **kw):
+        cursor = conn.cursor()
+        try:
+            cursor.execute("BEGIN")
+            retval = self.func(cursor, *args, **kw)
+            cursor.execute("COMMIT")
+        except Exception, e :
+            logging.warning('Exception in %s' % self.func)
+            template = "An exception of type {0} occured. Arguments:\n{1!r}"
+            message = template.format(type(e).__name__, e.args)
+            logging.exception(message)
+            cursor.execute("ROLLBACK")
+            retval = None
+        finally:
+            cursor.close()
+
+        return retval
+    
+
+def db_trx_exception_handler(func):
+    """Wrap a function in an idomatic SQL transaction.  The wrapped function
+    should take a cursor as its first argument; other arguments will be
+    preserved.
+    """
+
+    def new_func(conn, *args, **kwargs):
+
+        retval = None
+        cursor = conn.cursor()
+        try:
+            cursor.execute("BEGIN")
+            retval = func(cursor, *args, **kwargs)
+            cursor.execute("COMMIT")
+        except Exception, e:
+            cursor.execute("ROLLBACK")
+            logging.warning("Database Exception Handled:\n " + str(e))
+        finally:
+            cursor.close()
+
+        return retval
+
+    # Tidy up the help()-visible docstrings to be nice
+    new_func.__name__ = func.__name__
+    new_func.__doc__ = func.__doc__
+
+    return new_func
+
+
+def general_exception_handler(func):
+    
+    def new_func(config, *args, **kwargs):
+        retval = None
+        try:
+            retval = func(config, *args, **kwargs)
+        except Exception, e:
+            logging.warning("General Exception Handled:\n " + str(e))
+        
+        return retval
+
+    # Tidy up the help()-visible docstrings to be nice
+    new_func.__name__ = func.__name__
+    new_func.__doc__ = func.__doc__
+        
