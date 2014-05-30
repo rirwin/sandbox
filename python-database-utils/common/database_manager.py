8f258c1ca19dee16a8f6ef34401891534018db30
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-05-30 15:48:52 -0700

    Cleaned up a few things.  Added methods for table creation.

--- python-database-utils/common/database_manager.py
+++ python-database-utils/common/database_manager.py
@@ -6,37 +6,32 @@ import logging
 import wrappers
 
 
-@wrappers.db_trx_exception_handler
-def do_something(cursor, val1=1, val2=2):
-    #cursor.execute("select %s, %s", (val1, val2))
-    cursor.execute("drop table foo")
-    return cursor.fetchall()
-
 
 class database_manager:
 
     def __init__(self, db_config_path):
         self.load_config(db_config_path)
 
+    @wrappers.logger
+    @wrappers.general_function_handler
     def load_config(self, db_config_path):
         config = ConfigParser.ConfigParser()
         config.read(db_config_path)
+        logging.basicConfig(level = config.get("main", "loglevel") )
         
         # set up postgreSQL
-        dbprog = config.get("main", "dbprog")
-        if dbprog == "postgresql":
-            conn = self.init_psql_conn(config)
-            if conn is None:
+        self.dbprog = config.get("main", "dbprog")
+        if self.dbprog == "postgresql":
+            self.conn = self.init_psql_conn(config)
+            if self.conn is None:
                 logging.error("\nCannot open a connection to database. Exiting.\n")
                 sys.exit(1)
-            self.conn = conn
 
-            #database_manager.init_psql_conn_static(config)
             self.schema_loader(config)
 
-        logging.basicConfig(level = config.get("main", "loglevel") )
 
-
+    @wrappers.logger
+    @wrappers.general_function_handler
     def schema_loader(self, config):
         try:
             db_schema_path = config.get("main", "schemafile")
@@ -56,38 +51,57 @@ class database_manager:
 
         return conn
 
+    @wrappers.logger
+    @wrappers.general_function_handler
     def schema_loader(self, config):
-        try:
-            db_schema_path = config.get("main", "schemafile")
-            self.schema_dict = json.load(open(db_schema_path))
-        except Exception, e:
-            print("%s \nCannot load schema file %s. \n Exiting.\n" % (e, db_schema_path))
-            sys.exit(1)
+        db_schema_path = config.get("main", "schemafile")
+        self.schema_dict = json.load(open(db_schema_path))
 
 
+    @wrappers.logger
+    @wrappers.general_function_handler
     def reset_all_tables(self):
         for table_i in self.schema_dict:
             self.reset_table(table_i)
 
+    @wrappers.logger
+    @wrappers.general_function_handler
     def reset_table(self, table_str):
         self.drop_table(self.conn, table_str)
+        schema_arr = self.schema_dict[table_str]
+        schema_str = database_manager.translate_schema_array(self.dbprog, schema_arr)
+        self.create_table(self.conn, table_str, schema_str)
 
     # called by <referent>.drop_table(<referent>.conn, table_str)
     @wrappers.logger
     @wrappers.database_function_handler
     def drop_table(self, cursor, table_str):
-        #cursor.execute("drop table if exists " + table_str)
-        cursor.execute("drop table " + table_str) # force exception
-        return cursor.fetchall()[0][0]
+        cursor.execute("drop table if exists " + table_str) 
+
+    @wrappers.logger
+    @wrappers.database_function_handler
+    def create_table(self, cursor, table_str, schema_str):
+        cursor.execute("create table " + table_str + schema_str) 
+
+
+    @staticmethod
+    @wrappers.logger
+    @wrappers.general_function_handler
+    def translate_schema_array(dbprog, schema_arr):
+        if dbprog == "postgresql":
+            schema_str = "("
+            for col_i in schema_arr:
+                schema_str += "\"" + col_i[0] + "\" " + col_i[1] + ","
+        
+        return schema_str[:-1] + ")"
 
 
     # called by database_manager.drop_table_static(conn, table_str)
     @staticmethod
+    @wrappers.logger
     @wrappers.db_trx_exception_handler
     def drop_table_static(cursor, table_str):
-        #cursor.execute("drop table if exists " + table_str)
-        cursor.execute("drop table " + table_str) # force exception
-        return cursor.fetchall()[0][0]
+        cursor.execute("drop table if exists " + table_str)
 
  
 if '__main__' == __name__:
@@ -97,8 +111,6 @@ if '__main__' == __name__:
 
     dm = database_manager(db_config_path)
 
-    #print do_something(dm.conn)
-
     dm.reset_all_tables()
 
-    print "Program continues"
+    print "Program continued to end"
