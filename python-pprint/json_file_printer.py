b7963346fdb869458a8ad7e39339366b6e6f7748
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-06-07 12:26:56 -0700

    added a json file printer. has example file test.json

---
+++ python-pprint/json_file_printer.py
@@ -0,0 +1,26 @@
+import json
+from pprint import pprint as pprint
+import sys
+
+
+if len(sys.argv) != 2:
+    print "you need 1 argument to be path to the file (full or relative)"
+
+file_path = sys.argv[1]
+print "opening",file_path
+
+try:
+    file_handle = open(file_path)
+except Exception, e:
+    print "File", file_path, "not loaded"
+    print e
+    sys.exit(1)
+
+try:
+    json_dict = json.load(file_handle)
+except Exception, e:
+    print "Unable to JSON"
+    print e
+    sys.exit(1)
+
+pprint(json_dict)
