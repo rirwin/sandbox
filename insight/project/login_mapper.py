3f1cf37db18c379a95629e27396b58b60d6c7ddd
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-06-08 00:18:05 -0700

    Added a python map and reduce script

---
+++ insight/project/login_mapper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # emit login id
+    #try:
+    json_dict = json.loads(line)
+    print '%s\t%s' % (json_dict["actor_attributes"]["login"], 1)
+
+
+    """
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print '%s\t%s' % (word, 1)
+    """
