ed03595ff801965a57af4b7178dd81b85044cff1
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-06-07 21:09:29 -0700

    Github "json" printer.  Github provides an array of json dictionaries with no commas or outside [] braces.  This code allows loading each dictionary to an array of dictionaries.

---
+++ python-pprint/github_json_printer.py
@@ -0,0 +1,50 @@
+import json
+from pprint import pprint as pprint
+import sys
+
+
+if len(sys.argv) != 2:
+    print "you need 1 argument to be path to the file (full or relative)"
+    sys.exit(1)
+
+file_path = sys.argv[1]
+print "opening",file_path
+
+try:
+    f = open(file_path)
+except Exception, e:
+    print "File", file_path, "not loaded"
+    print e
+    sys.exit(1)
+
+arr_of_dicts = []
+
+def read_next_dictionary(f):
+    depth = 0
+    json_str = ""
+    while True:
+        c = f.read(1)
+        if not c:
+            break #EOF
+        json_str += str(c)
+        if c == '{':
+            depth += 1
+        elif c == '}':
+            depth -= 1
+
+        if depth == 0:
+            break
+
+    return json_str
+
+
+while True:
+    json_as_str = read_next_dictionary(f)
+    try:
+        json_dict = json.loads(json_as_str)
+        arr_of_dicts.append(json_dict)
+    except: 
+        break # exception on loading json to end loop
+
+pprint(arr_of_dicts)
+
